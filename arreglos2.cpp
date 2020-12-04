f74b909e98098ed4d18032a98d5d1062ad4a7b9d
belsymena <74078935+belsymena@users.noreply.github.com>
2020-12-04 18:43:33 -0500

    Delete arreglos2.cpp

--- arreglos2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-using namespace std;
-int main(int argc, char** argv) {
-	int array[10]={0};
-	return 0;
-}
